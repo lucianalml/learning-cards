4a7315d70b142709768f7401d520351074674d57
Luciana Lima <lucianalima.lml@gmail.com>
2017-02-19 20:58:20 -0300

    Sidenav check

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  lessons = [
+    { title: 'Lesson 1',
+      completed: false },
+    { 
+      title: 'Lesson 2',
+      completed: false 
+    }
+  ];
+
+  concluded = 0;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.lessons);
+
+    this.concluded = (100 * this.lessons.filter(lesson => lesson.completed).length) / this.lessons.length;
+
+  }
+
+  checkLesson(i: number){
+    this.lessons[i].completed = ! this.lessons[i].completed;
+    this.concluded = (100 * this.lessons.filter(lesson => lesson.completed).length) / this.lessons.length;
   }
 
 }
